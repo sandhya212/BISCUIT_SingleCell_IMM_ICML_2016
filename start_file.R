02a472c4d76b633e85741ef8d6d7665848b2380b
Sandhya Prabhakaran <sandhyaprabhakaran@gmail.com>
2017-05-25 16:02:34 -0400

    Updated start file to cater to single gene batch versus multi gene batches.

--- start_file.R
+++ start_file.R
@@ -46,19 +46,19 @@ if(! dir.exists(paste0(getwd(),"/output"))){
 
 input_file_name <- "expression_mRNA_17-Aug-2014.txt";
 
-choose_cells <- 1500; #comment if you want all the cells to be considered
+choose_cells <- 1050; #comment if you want all the cells to be considered
 
-choose_genes <- 100; #comment if you want all the genes to be considered
+choose_genes <- 150; #comment if you want all the genes to be considered
 
-gene_batch <- 20; #number of genes per batch, therefore num_batches = choose_genes (or numgenes)/gene_batch. Max value is 150
+gene_batch <- 50; #number of genes per batch, therefore num_batches = choose_genes (or numgenes)/gene_batch. Max value is 150
 
-num_iter <- 10; #number of iterations, choose based on data size.
+num_iter <- 15; #number of iterations, choose based on data size.
 
-num_cores <- detectCores() - 1; #number of cores for parallel processing. Ensure that detectCores() > 1 for parallel processing to work, else set num_cores to 1.
+num_cores <- detectCores() - 4; #number of cores for parallel processing. Ensure that detectCores() > 1 for parallel processing to work, else set num_cores to 1.
 
 z_true_labels_avl <- TRUE; #set this to TRUE if the true labels of cells are available, else set it to FALSE. If TRUE, ensure to populate 'z_true' with the true labels in 'BISCUIT_process_data.R'
 
-num_cells_batch <- 1000; #set this to 1000 if input number of cells is in the 1000s, else set it to 100. 
+num_cells_batch <- 1000; #set this to 1000 if input number of cells is in the 1000s, else set it to 100.
 
 ## call BISCUIT
 source("BISCUIT_main.R")
